8c8b2ff59e3b3e00fa7de9f06df9772939ac78a3
Gabor Szabo <szabogabi@gmail.com>
2016-09-12 17:35:23 +0200

    ujvektoros elemek

--- templates/home-inspired.php
+++ templates/home-inspired.php
@@ -1,7 +1,7 @@
 <img src="<?= get_stylesheet_directory_uri(); ?>/dist/images/6panels_color.jpg" alt="Inspired">
 <div class="row">
   <div class="columns medium-8 medium-push-4 tablet-6 tablet-push-6 large-4 large-push-8">
-    <h2 class="styleist__title">Inspired</h3>
+    <h2 class="styleist__title">Inspired</h2>
     <div class="styleist__text">results<br>people<br>work<br>clients<br>talent</div>
   </div>
 </div>
