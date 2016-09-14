0621fc6f0b664fd25b86317df3137cf8febc92b7
Gabor Szabo <szabogabi@gmail.com>
2016-09-14 17:36:13 +0200

    teaser proto

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -18,7 +18,54 @@ if ( $('body').hasClass('page-template-tmpl-landing') ) {
   Pace.on('done', function() {
     $('.thecover').addClass('opening');
     $('.top-bar').removeClass('darkened');
-    window.location.href = $('.homelogo').data('realhome');
+    //window.location.href = $('.homelogo').data('realhome');
+    $('.thecover').addClass('rejtve');
+    $('.typewriter').typed({
+      strings: ['The bespoke creative agency', 'delivering Inspired original design', 'to', 'The commercial real estate world'],
+      // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+      stringsElement: null,
+      // typing speed
+      typeSpeed: 10,
+      // time before typing starts
+      startDelay: 0,
+      // backspacing speed
+      backSpeed: 0,
+      // shuffle the strings
+      shuffle: false,
+      // time before backspacing
+      backDelay: 500,
+      // loop
+      loop: false,
+      // false = infinite
+      loopCount: false,
+      // show cursor
+      showCursor: true,
+      // character for cursor
+      cursorChar: "|",
+      // attribute to type (null == text)
+      attr: null,
+      // either html or text
+      contentType: 'html',
+
+      // call when done callback function
+      callback: function() {
+        $('.top-bar').addClass('darkened');
+        $('.document').addClass('docfade');
+        window.location.href = $('.homelogo').data('realhome');
+      },
+
+      // starting callback function before each string
+      preStringTyped: function() {},
+
+      //callback for every typed string
+      onStringTyped: function() {},
+
+      // callback for reset
+      resetCallback: function() {}
+    });
+
+
+
   });
 } else {
   Pace.on('done', function() {
--- tmpl-landing.php
+++ tmpl-landing.php
@@ -5,11 +5,11 @@
 ?>
 
 <?php while (have_posts()) : the_post(); ?>
-  <div class="ps">
+  <div class="ps lala">
     <div class="row">
       <div class="columns">
         <div class="section teaser">
-          <?php the_content(); ?>
+          <a href="<?php the_permalink(45); ?>" class="typewriter"></a>
         </div>
       </div>
     </div>
